1db5f27bb73e55bdfc1d3e58451f432fa924be44
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-18 18:35:40 +0100

    Load the goat image in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <h1>Doc ta Chèvre</h1>
     <p>La DOC de l'API de l'APLI</p>
+    <Image fluid={data.file.childImageSharp.fluid} />
   </Layout>
 )
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "goat-book.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1024) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
