08a583824998995be94021ad2bd690c18a055330
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-19 13:13:01 +0100

    Add multiple menu items for testing

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,7 +5,13 @@ import MenuItem from "./MenuItem"
 export default () => (
   <ul className="main-menu">
     <li>
-      <MenuItem to="/plop">plop</MenuItem>
+      <MenuItem to="/plop">plop oui</MenuItem>
+    </li>
+    <li>
+      <MenuItem to="/plop2">plop2</MenuItem>
+    </li>
+    <li>
+      <MenuItem to="/plop3">plop3et plop</MenuItem>
     </li>
   </ul>
 )
