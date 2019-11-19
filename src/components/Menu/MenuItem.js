d9c2eb91cc1005799da0e303dce34d2e3f703cd2
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-19 13:58:15 +0100

    Set active class on an active MenuItem

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -2,8 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import "./MenuItem.css"
 
+const isActive = ({ isCurrent }) =>
+  isCurrent ? { className: "menu-item active" } : null
+
 export default ({ children, ...rest }) => (
-  <Link {...rest} className="menu-item">
+  <Link {...rest} className="menu-item" getProps={isActive}>
     {children}
   </Link>
 )
