5f9fd8c97abd9b041dd7ae5ec2e4ae8aa7dc12a5
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-19 13:33:42 +0100

    Edit menu item colors

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -2,4 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import "./MenuItem.css"
 
-export default ({ children, ...rest }) => <Link {...rest}>{children}</Link>
+export default ({ children, ...rest }) => (
+  <Link {...rest} className="menu-item">
+    {children}
+  </Link>
+)
