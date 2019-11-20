de31f5f6e400479a4521c6d10a852e86f819b31f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-20 14:06:31 +0100

    Load features

--- src/pages/features.js
+++ src/pages/features.js
@@ -1,9 +1,37 @@
 import React from "react"
 import Layout from "../components/Layout"
-import PageNotReady from "../components/PageNotReadyYet"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <PageNotReady />
+    <div className="features">
+      {data.allFile.features.map(feature => (
+        <div
+          key={feature.data.id}
+          dangerouslySetInnerHTML={{ __html: feature.data.html }}
+        ></div>
+      ))}
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "markdown" }
+        relativeDirectory: { eq: "features" }
+      }
+    ) {
+      features: nodes {
+        data: childMarkdownRemark {
+          id
+          frontmatter {
+            title
+          }
+          html
+        }
+      }
+    }
+  }
+`
