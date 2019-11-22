b469628e7bf70543ae55e545f0f57984e41f9a64
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-22 14:12:11 +0100

    Add links to created feature pages

--- src/pages/features.js
+++ src/pages/features.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 export default ({ data }) => (
   <Layout>
     <div className="features">
-      {data.allFile.features.map(feature => (
-        <div
-          key={feature.data.id}
-          dangerouslySetInnerHTML={{ __html: feature.data.html }}
-        ></div>
+      {data.allFile.features.map(({ data: feature }) => (
+        <div key={feature.id}>
+          <h2>
+            <Link to={feature.fields.slug}>{feature.frontmatter.title}</Link>
+          </h2>
+          <div dangerouslySetInnerHTML={{ __html: feature.html }}></div>
+        </div>
       ))}
     </div>
   </Layout>
@@ -28,6 +30,9 @@ export const query = graphql`
           id
           frontmatter {
             title
+          }
+          fields {
+            slug
           }
           html
         }
