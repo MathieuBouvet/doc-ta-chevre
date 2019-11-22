57c28989d21e4a3c7213a5110ee3eaf1c779e432
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-22 22:28:48 +0100

    Fix mistake in path name

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cache",
       options: {
-        extraDirsToCache: ["/public/static"],
+        extraDirsToCache: ["public/static"],
       },
     },
     {
