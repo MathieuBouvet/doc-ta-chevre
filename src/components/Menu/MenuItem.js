03aa7e20840b2d74ede991e7372ef47a2680afb9
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-22 14:14:23 +0100

    Change active menu to display on partially current

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import "./MenuItem.css"
 
-const isActive = ({ isCurrent }) =>
-  isCurrent ? { className: "menu-item active" } : null
+const isActive = ({ isPartiallyCurrent }) =>
+  isPartiallyCurrent ? { className: "menu-item active" } : null
 
 export default ({ children, ...rest }) => (
   <Link {...rest} className="menu-item" getProps={isActive}>
